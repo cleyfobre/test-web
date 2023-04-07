e89bab6ca24fa273874572ac0dd7708107b9ba66
Young <cleyfobre@gmail.com>
2023-04-07 15:20:59 +0900

    Add setting.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
 # blue up if no blue, green up if blue
-HELLO=$(pwd)
-logger "pwd?"
-logger ${HELLO}
-
-NO_BLUE=$(docker compose -p test-web-blue -f docker-compose.blue.yml ps | grep Up)
+NO_BLUE=$(sudo docker compose -p test-web-blue -f docker-compose.blue.yml ps | grep Up)
 if [ -z "$NO_BLUE" ]; then
     logger "blue up"
-    docker compose -p test-web-blue -f docker-compose.blue.yml up -d
+    sudo docker compose -p test-web-blue -f docker-compose.blue.yml up -d
     logger "blue up2"
     BEFORE_COMPOSE_COLOR="green"
     AFTER_COMPOSE_COLOR="blue"
     logger "blue up3"
 else
     logger "green up"
-    docker compose -p test-web-green -f docker-compose.green.yml up -d
+    sudo docker compose -p test-web-green -f docker-compose.green.yml up -d
     logger "green up2"
     BEFORE_COMPOSE_COLOR="blue"
     AFTER_COMPOSE_COLOR="green"
@@ -24,14 +20,14 @@ fi
 
 # check if new container is still up after 10s
 sleep 10s
-IS_UP=$(docker compose -p test-web-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
+IS_UP=$(sudo docker compose -p test-web-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yml ps | grep Up)
 if [ -n "$IS_UP" ]; then
 
     # if no nginx, run new nginx container
-    NO_NGINX=$(docker ps | grep test-nginx)
+    NO_NGINX=$(sudo docker ps | grep test-nginx)
     if [ -z "$NO_NGINX" ]; then
-        docker image build -t test-nginx:0.0.1 -f Dockerfile_nginx . --build-arg COMPOSE_COLOR="${AFTER_COMPOSE_COLOR}"
-        docker run -d -p 80:80 --name test-nginx test-nginx:0.0.1
+        sudo docker image build -t test-nginx:0.0.1 -f Dockerfile_nginx . --build-arg COMPOSE_COLOR="${AFTER_COMPOSE_COLOR}"
+        sudo docker run -d -p 80:80 --name test-nginx test-nginx:0.0.1
         logger "nginx started"
     else
         docker cp ./nginx.${AFTER_COMPOSE_COLOR}.conf test-nginx:/etc/nginx/nginx.conf
@@ -40,6 +36,6 @@ if [ -n "$IS_UP" ]; then
     fi
 
     # previous app down
-    docker compose -p test-web-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
+    sudo docker compose -p test-web-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yml down
     logger "$BEFORE_COMPOSE_COLOR down"
 fi
